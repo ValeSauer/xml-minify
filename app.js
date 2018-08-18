ff654c58df451b3d2bde81646fa8007f1693c7c7
Valentin <valentin@sauer-medientechnik.de>
2018-08-18 22:06:30 +0200

    Changed XML Writer Library

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-var fs        = require('fs')
-var XmlStream = require('xml-stream') ;
-var readStream = fs.createReadStream('test_big.xml');
-//var readStream = fs.createReadStream('test.xml');
+const fs        = require('fs')
+const XmlStream = require('xml-stream') ;
+const readStream = fs.createReadStream('test.xml');
 const writeStream = fs.createWriteStream('out.xml');
-var xmlIn = new XmlStream(readStream);
+const xmlIn = new XmlStream(readStream);
+const _ = require('lodash');
 
 var xml = require('xml');
 
 xmlIn.preserve('PGR PRO', false);
 
-xmlIn.on('updateElement: PRO', function(readProduct) {
+configs = [{
+  level: 1,
+  filterNode: 'PRAT',
+  attributeFilters: [{name: 'hoehe'}, {name: 'breite'}],
+  keepAttributes: ['name'],
+  flatten: false
+},{
+  level: 1,
+  filterNode: 'PRV',
+  attributeFilters: null,
+  keepAttributes: ['id'],
+  flatten: false
+},{
+  level: 2,
+  filterNode: 'PRAT',
+  attributeFilters: [{name: 'hoehe'}, {name: 'breite'}],
+  keepAttributes: ['name'],
+  flatten: false
+},{
+  level: 3,
+  filterNode: 'VALUE',
+  attributeFilters: null,
+  keepAttributes: [],
+  flatten: true
+}];
 
+
+createProduct = function(readProduct){
   var writeProduct = [{
     PRO: [{
       _attr: {
@@ -19,44 +45,60 @@ xmlIn.on('updateElement: PRO', function(readProduct) {
     }]
   }]
 
-  getVariants = function(writeProduct, readVariants){
-    readVariants.forEach(readVariant => {
-      if(readVariant.$name == "PRV"){
-        writeVariant = { 
-          PRV: [{
-            _attr: {
-              id: readVariant.$.id
-            }
-          }]
-        }
-        writeVariant = getChildren(writeVariant, readVariant.$children);
-        writeProduct[0].PRO.push(writeVariant);
-      }   
-    })
-    return writeProduct;
-  }
+ newChildern = getChildren(readProduct.$children);
+ newChildern.forEach(newChild => {
+  writeProduct[0].PRO.push(newChild);
+ })
 
-  getChildren = function(writeVariant, readChildren){
-    readChildren.forEach(readChild => {
-      if(readChild.$name == "PRAT" && (readChild.$.name == "hoehe" || readChild.$.name == "breite")){
-        writeChild = { 
-          PRAT: [{
-            _attr: {
-              name: readChild.$.name
-            }
-          },
-          readChild.$text]
-        }
-        writeVariant.PRV.push(writeChild);
-      }   
-    })
-    return writeVariant;
-  }
-
-  writeProduct = getVariants(writeProduct, readProduct.$children);
-
+  console.log(xml(writeProduct, true));
+  
   writeStream.write(xml(writeProduct, true));
   writeStream.write('\n');
+
+}
+
+getChildren = function(readChildren, level = 0){
+  level++;
+  writeChildren = [];
+  readChildren.forEach(readChild => {
+    var localConfig = _.find(configs, config => {        
+      if(config.level == level && config.filterNode == readChild.$name){
+        return true;
+      }
+    })
+    if(localConfig){
+      var attributes = {}
+      localConfig.keepAttributes.forEach(keepAttribute => {
+        attributes[keepAttribute] = readChild.$[keepAttribute];
+      })
+      var writeChild = {};
+      writeChild[readChild.$name] = [];
+      writeChild[readChild.$name].push({_attr: attributes});
+      if(_.isArray(readChild.$children)){
+        if(_.isString(readChild.$children[0])){
+          console.log("Appending Value " + readChild.$children[0] + " to node " + readChild.$name)
+          writeChild[readChild.$name].push(readChild.$children[0]);
+        }else if (_.isObject(readChild.$children[0]) && 1 == 2){
+          console.log("Appending Node")
+          newChildren = getChildren(readChild.$children, level);
+          newChildren.forEach(newChild => {
+            writeChild[readChild.$name].push([{newChild}]);
+          })
+        }
+      }
+      writeChildren.push(writeChild);
+    }
+  })
+  return writeChildren;
+}
+
+xmlIn.on('updateElement: PRO', function(readProduct) {
+  try {
+    createProduct(readProduct);
+  }
+  catch (e) {
+   console.error(e);
+  }
 
 });
 
@@ -68,11 +110,9 @@ writeStream.on('finish', () => {
 // close the stream
 
 
-/*
 xmlIn.on('end', function(){
-  writeStream.end();
-  xmlOut.end(writer);
-})*/
+  console.log("End of Read Stream")
+})
 
 
 
